e2adb4958f0fd03c20c2fc04ce581798c7e424be
Ishaan Sensharma <ishaans.official01@gmail.com>
2026-02-22 20:13:59 +0530

    updated final v3.1

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 
-// Background script for NanoGPT Agent
-// This script listens for messages from the side panel and executes commands in the context of the active tab.
+// new changes
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
